7be00eed4e25e57cbe8006cb57a6fd39d00731c2
icmru <120552561+icmru@users.noreply.github.com>
2023-07-07 15:52:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function login() {
+  var username = document.querySelector('input[type="text"]').value;
+  var password = document.querySelector('input[type="password"]').value;
+  console.log("账号：" + username);
+  console.log("密码：" + password);
+}
